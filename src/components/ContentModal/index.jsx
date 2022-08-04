7caed01e684d2b055114db81064cf85b3be390a6
Linh <lixxkook@Macbut-M1-cua-Linh.local>
2022-08-04 23:42:59 +0700

    upadte

--- src/components/ContentModal/index.jsx
+++ src/components/ContentModal/index.jsx
@@ -3,26 +3,6 @@ import './ContentModal.scss'
 
 const content = [
   {
-    name:"story",
-    title:"STORY",
-    content:"Soccer Realm is the most diverse ecosystem specifically designed for football fans around the globe. We want the blockchain application to bring the best experience to football fans. In the fantasy universe of Soccer Realm, you will freely play with your passion for this sport, feeling it in many different ways."
-  },
-  {
-    name:"gameplay",
-    title:"GAME PLAY",
-    content:"Soccer Realm is the most diverse ecosystem specifically designed for football fans around the globe. We want the blockchain application to bring the best experience to football fans. In the fantasy universe of Soccer Realm, you will freely play with your passion for this sport, feeling it in many different ways."
-  },
-  {
-    name:"roadmap",
-    title:"ROAD MAP",
-    content:"Q2-2022 Ideas for the project Building a technology platform The graphic design of the project Whitepaper v1 Q3-2022 Completing the website Seed-round Testnet project Private-sale on Pinksale Organic Community Growth Project mainnet Q4-2022 Public-sale Listing Coinmarketcap Listing Coingecko Launch-NFT Open the staking feature. Open the project's Marketplace Open the match prediction feature of the World Cup event. Impact Marketing Q1-2023 Audit More Partnerships Launching Football Manager game Building the foundation to develop Metaverse Listing Cex Burning tokens Launching the NFT Material Metaverse Building Whitepaper v2 Q2-2023 Launching NFT Izan On-game VR device testing Launching NFT Lean Tournament feature"
-  },
-  {
-    name:"about",
-    title:"ABOUT",
-    content:"Each gemstone is crystallized for up to billions of years. Created from the quintessence of heaven and earth, each piece has a different beauty. No two are the same. The added bonus is the skill of the artisans, which will enhance the value of that gem."
-  },
-  {
     name:"tokenomic",
     title:"TOKENOMIC",
     content:"Token:Soccer Realm Symbol : SOR Chain: BSC Total Supply: 1.000.000.000 SOR. SOR is a token deployed on the Binance Smart Chain. It is used in the ecosystem of the Soccer Realm. Giving voting rights to owners to decide the direction of development of the Soccer Realm ecosystem To join the NFT exchange market, log in to the game, and generate passive income through staking."
@@ -30,27 +10,205 @@ const content = [
 ]
 
 export default function ContentModal({status,name}) {
+
+  console.log(name)
   
   return (
-    <div className={`content-modal ${status}`}>
+    
+    <>
       {
-        content.map((item,index) => {
-          if(name===item.name){
-            return (
-              <div key={index} className="content-modal-wrapper">
-                <div className="content-modal-title">
-                  {item.title}
-                </div>
+        name === "story"
+        &&
+        <div className={`content-modal ${status}`}>
+          <div className="content-modal-wrapper">
+            <div className="content-modal-title primary-gradient-text">
+              STORY
+            </div>
 
-                <div className="content-modal-main">
-                  {item.content}
-                </div>
+            <div className="content-modal-main">
+              <div className="content-modal-li">
+                <span className="primary-gradient-text">
+                  Football Wonderland 
+                </span>
+                 is the most diverse ecosystem specifically designed for football fans around the globe.
               </div>
-            )
-          }
-        })
+              <div className="content-modal-li">
+                We want the blockchain application to bring the best experience to football fans. 
+              </div>
+              <div className="content-modal-li">
+                In the fantasy universe of <span className="primary-gradient-text">
+                  Football Wonderland 
+                </span>, you will freely play with your passion for this sport, feeling it in many different ways.
+              </div>
+            </div>
+          </div>
+        </div>
       }
-      
-    </div>
+
+      {
+        name === "about"
+        &&
+        <div className={`content-modal ${status}`}>
+          <div className="content-modal-wrapper">
+            <div className="content-modal-title primary-gradient-text">
+              ABOUT
+            </div>
+
+            <div className="content-modal-main">
+              <div className="content-modal-li">
+                Each gemstone is crystallized for up to 
+                <span className="primary-gradient-text">
+                  billions of years.
+                </span>
+              </div>
+              <div className="content-modal-li">
+                No two are the same. The added bonus is the skill of the artisans, which will enhance the value of that gem.
+              </div>
+            </div>
+          </div>
+        </div>
+      }
+
+      {
+        name === "gameplay"
+        &&
+        <div className={`content-modal ${status}`}>
+          <div className="content-modal-wrapper">
+            <div className="content-modal-title primary-gradient-text">
+              GAMEPLAY
+            </div>
+
+            <div className="content-modal-main">
+              <div className="content-modal-li">
+                <span className="primary-gradient-text">
+                  Football Wonderland 
+                </span> is the most diverse ecosystem specifically designed for football fans around the globe.
+              </div>
+              <div className="content-modal-li">
+                We want the blockchain application to bring the best experience to football fans.
+              </div>
+               <div className="content-modal-li">
+                In the fantasy universe of 
+                <span className="primary-gradient-text">
+                  Football Wonderland 
+                </span>
+                , you will freely play with your passion for this sport, feeling it in many different ways.
+              </div>
+            </div>
+          </div>
+        </div>
+      }
+
+      {
+        name === "roadmap"
+        &&
+        <div className={`content-modal ${status}`}>
+          <div className="content-modal-wrapper">
+            <div className="content-modal-title primary-gradient-text">
+              ROAD MAP
+            </div>
+
+            <div className="content-modal-main">
+              <div className="roadmap-item roadmap-item--left">
+                <span className="primary-gradient-text">
+                 <i className="fa-solid fa-angles-right"></i> Q2-2022 
+                </span>
+                <span>
+                  Ideas for the project Building a technology platform The graphic design of the project Whitepaper v1
+                </span>
+              </div>
+
+              <div className="roadmap-item roadmap-item--right">
+                <span className="primary-gradient-text">
+                 <i className="fa-solid fa-angles-right"></i> Q3-2022
+                </span>
+                <span>
+                  Completing the website Seed-round Testnet project Private-sale on Pinksale Organic Community Growth Project mainnet
+                </span>
+              </div>
+
+              <div className="roadmap-item roadmap-item--left">
+                <span className="primary-gradient-text">
+                 <i className="fa-solid fa-angles-right"></i> Q4-2022 
+                </span>
+                <span>
+                  Public-sale Listing Coinmarketcap Listing Coingecko Launch-NFT Open the staking feature. Open the project's Marketplace Open the match prediction feature of the World Cup event. Impact Marketing
+                </span>
+              </div>
+
+              <div className="roadmap-item roadmap-item--right">
+                <span className="primary-gradient-text">
+                 <i className="fa-solid fa-angles-right"></i> Q1-2023
+                </span>
+                <span>
+                  Audit More Partnerships Launching Football Manager game Building the foundation to develop Metaverse Listing Cex Burning tokens Launching the NFT Material Metaverse Building Whitepaper v2
+                </span>
+              </div>
+
+              <div className="roadmap-item roadmap-item--left">
+                <span className="primary-gradient-text">
+                 <i className="fa-solid fa-angles-right"></i> Q2-2023 
+                </span>
+                <span>
+                  Launching NFT Izan On-game VR device testing Launching NFT Lean Tournament feature
+                </span>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      }
+
+      {
+        name === "tokenomic"
+        &&
+        <div className={`content-modal ${status}`}>
+          <div className="content-modal-wrapper">
+            <div className="content-modal-title primary-gradient-text">
+              TOKENOMIC
+            </div>
+
+            <div className="tokenomic-img">
+              <img src="./images/token.png" alt="" />
+            </div>
+
+            <div className="content-modal-main content-modal-main--tokenomic">
+              <div className="content-modal-li">
+                <div className="content-modal-li-title">
+                  Token
+                </div>
+                <span className="primary-gradient-text">
+                  : Football Wonderland.
+                </span>
+              </div>
+
+              <div className="content-modal-li">
+                <div className="content-modal-li-title">
+                  Symbol
+                </div>
+                <span className="primary-gradient-text">
+                  : SOR
+                </span>
+              </div>
+
+              <div className="content-modal-li">
+                <div className="content-modal-li-title">
+                  Total Supply
+                </div>
+                <span className="primary-gradient-text">
+                  : 1.000.000.000
+                </span>
+              </div>
+
+              <div className="content-modal-li">
+                SOR is a token deployed on the Binance Smart Chain. It is used to join the NFT exchange market, log in to the game, and generate passive income through staking.
+              </div>
+
+            </div>
+          </div>
+        </div>
+      }
+
+    </>
   )
 }
