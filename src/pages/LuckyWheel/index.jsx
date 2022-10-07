b05098953d3dfad4f562e847a2b63efbac2acf89
Linh <lixxkook@Macbut-M1-cua-Linh.local>
2022-10-07 23:41:24 +0700

    dcmm con cho vong xoay ne co gi la kho voi bo ha

--- src/pages/LuckyWheel/index.jsx
+++ src/pages/LuckyWheel/index.jsx
@@ -1,16 +1,70 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './LuckyWheel.scss'
 
-import PureWheel from '../../components/PureWheel'
-
 export default function LuckyWheel() {
+
+  const [rotateState,setRotateState] = useState('')
+  const [resultNumber,setResultNumber] = useState('')
+
+  
+  const handleSpinning = () => {
+    const resultNum = Math.floor(Math.random() * 7) + 1;
+    setResultNumber('')
+
+    // begin
+    setRotateState('active')
+
+    // finish
+    setTimeout(() => {
+      setRotateState('')
+      setResultNumber(resultNum)
+      
+      console.log("Trung so :",resultNum);
+    },1700+resultNum*50)
+  }
+
   return (
     <div className="lucky-wheel centering">
       <div className="lucky-wheel-wrapper">
         <div className="lucky-wheel-tower">
           <img className="tower-wheel" src="/images/2/wheel.png" alt="" />
 
-          
+          <div className={`wheel-prize ${rotateState}`}>
+            <div className={`wheel-prize-item wheel-prize-item--1 ${ resultNumber === 1 && 'active'}`}>
+              <img src="./images/2/usd.png" alt="" className="wheel-prize-icon" />
+            </div>
+
+            <div className={`wheel-prize-item wheel-prize-item--2 ${ resultNumber === 2 && 'active'}`}>
+              <img src="./images/2/b.png" alt="" className="wheel-prize-icon" />
+            </div>
+            
+            <div className={`wheel-prize-item wheel-prize-item--3 ${ resultNumber === 3 && 'active'}`}>
+              <img src="./images/2/Binance.png" alt="" className="wheel-prize-icon" />
+            </div>
+            
+            <div className={`wheel-prize-item wheel-prize-item--4 ${ resultNumber === 4 && 'active'}`}>
+              <img src="./images/2/bitcoin.png" alt="" className="wheel-prize-icon" />
+            </div>
+
+            <div className={`wheel-prize-item wheel-prize-item--5 ${ resultNumber === 5 && 'active'}`}>
+              <img src="./images/2/usd.png" alt="" className="wheel-prize-icon" />
+            </div>
+
+            <div className={`wheel-prize-item wheel-prize-item--6 ${ resultNumber === 6 && 'active'}`}>
+              <img src="./images/2/T.png" alt="" className="wheel-prize-icon" />
+            </div>
+
+            <div className={`wheel-prize-item wheel-prize-item--7 ${ resultNumber === 7 && 'active'}`}>
+              <img src="./images/2/Binance.png" alt="" className="wheel-prize-icon" />
+            </div>
+
+            <div className={`wheel-prize-item wheel-prize-item--8 ${ resultNumber === 8 && 'active'}`}>
+              <img src="./images/2/bitcoin.png" alt="" className="wheel-prize-icon" />
+            </div>
+            
+            <img onClick={handleSpinning} className="wheel-button" src="./images/chongdien.jpeg" alt="" />
+          </div>
+
         </div>
 
         <div className="lucky-wheel-infor">
